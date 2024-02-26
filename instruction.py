1d1358ad6e3edf068c14ae710a976cc874f7ebdc
Roman1R <635roman.r642@gmail.com>
2024-02-26 20:40:59 +1000

    release

--- instruction.py
+++ instruction.py
@@ -1,37 +1,39 @@
-"""CSS-STYLES:
-h1 {
-   color: #d22e3a
-}
-
-h2 {
-   color: #ff01ff
-}
-"""
-
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/image_mars')
-def sample():
-    lst = """Человечество вырастает из детства.
-Человечеству мала одна планета.
-Мы сделаем обитаемыми безжизненные пока планеты.
-И начнем с Марса!
-Присоединяйся!""".split('\n')
-    return f"""<!doctype html>
+@app.route('/promotion_image')
+def promotion_image():
+    return """<!doctype>
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-    <title>Привет, Марс!</title>
+    <link rel="stylesheet" type="text/css" href="/static/css/style.css" />
+    <link rel="stylesheet" 
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+crossorigin="anonymous">
+    <title>Колонизация!</title>
 </head>
 <body>
     <h1>Жди нас, Марс!</h1>
-    <img src="{url_for('static', filename='img/mars.png')}" 
-           alt="АХТУНГ!">
-    
+    <img src="/static/img/mars.png" alt="АХТУНГ!">
+    <div class="alert alert-primary" role="alert">
+    Человечество вырастает из детства.
+    </div>
+    <div class="alert alert-secondary" role="alert">
+    Человечеству мала одна планета.
+    </div>
+    <div class="alert alert-success" role="alert">
+    Мы сделаем обитаемыми безжизненные пока планеты.
+    </div>
+    <div class="alert alert-danger" role="alert">
+    И начнем с Марса!
+    </div>
+    <div class="alert alert-warning" role="alert">
+    Присоединяйся!
+    </div>
 </body>
 </html>"""
 
